6d97bd0544da995096e0ea4bb236d31953d4d71b
shreyasraj-tech <shreyas.raj@techolution.com>
2025-11-03 18:54:19 +0000

    first code run

--- node-basics/server.js
+++ node-basics/server.js
@@ -1,4 +1,14 @@
-import https from https;
+import https from 'http';
 
-const server = https.createServer((req,res));
+const server = https.createServer((req,res) =>{
+    if(req.url === '/'){
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.end('Hello World from Node http module\n');
+        return;
+        }
+        res.writeHead(404);
+        res.end('Not found');
+});
 
+const PORT= 3000;
+server.listen(PORT, ()=> console.log(` Server running : ${PORT}`))
